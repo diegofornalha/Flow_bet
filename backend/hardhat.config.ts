c5f404877c6d1257ef9fa6901bcd311ea2214e1a
Diego Fornalha <diegofornalha@gmail.com>
2024-11-09 22:46:40 -0300

    atualizado

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,9 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-const { vars } = require("hardhat/config");
-const FLOW_PRIVATE_KEY = vars.get("FLOW_PRIVATE_KEY")
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const FLOW_PRIVATE_KEY = process.env.FLOW_PRIVATE_KEY!;
+
 const config: HardhatUserConfig = {
-    solidity: "0.8.24",
+    solidity: {
+        version: "0.8.28",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
+    },
     networks: {
       testnet: {
         url: `https://testnet.evm.nodes.onflow.org`,
